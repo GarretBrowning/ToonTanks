7e3abab33e8a709682559f3b3cbe11634e278948
Garret Browning <garret.browning@uj.edu>
2021-04-30 21:23:47 -0500

    Setting Up Pawn Components
    Created the Capsule Component, BaseMesh, TurretMesh, ProjectileSpawnPoint for each Pawn Component.

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -6,24 +6,35 @@
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
+class UCapsuleComponent;
+
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
+private:
+
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComp; // General Collision information for our pawns.
+	UPROPERTY()	
+	UStaticMeshComponent* BaseMesh; // Tank/A.I. base body
+	UPROPERTY()	
+	UStaticMeshComponent* TurretMesh; // Turret Mesh for both Tank & A.I.
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint; // Collection of data to present visual representation of where the projectile spawns from.
+ 
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
